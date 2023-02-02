03918d12a1f2f1af82ff69fe42967367177ccdd1
ParimSsme <parima.ssme@gmail.com>
2023-02-02 19:58:30 +0430

    remove TODOs

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -13,21 +13,16 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
 
-  //TODO: 2. Remove everything from the coinsList
   List<CoinModel> coinsList = [];
 
-  //TODO: 3. Create a function of type Future, called getCoinsValue
   Future getCoinsValue() async {
-    //TODO: 3.1 Use a try and catch block just make sure the code won't crash your app. Use the following links to learn more about Exception Handling in Dart.
     // https://medium.com/run-dart/dart-dartlang-introduction-exception-handling-f9f088906f7c
     // https://www.tutorialspoint.com/dart_programming/dart_programming_exceptions.htm
     try {
 
-      //TODO: 3.2 create a variable called data and assign it to what getCoinData from CoinData class returns
       CoinData coinData = CoinData();
       var data = await coinData.getCoinData(selectedCurrency);
 
-      //TODO: 3.3 If the data was not null, call the setState function, and inside the function assign data to the coins List.
       if (data!=null){
         setState((){
           coinsList = data;
@@ -39,7 +34,6 @@ class _PriceScreenState extends State<PriceScreen> {
     }
   }
 
-  //TODO: 4. override the initState function, and inside the function call the getCoinsValue function.
   @override
   void initState() {
     super.initState();
@@ -54,10 +48,8 @@ class _PriceScreenState extends State<PriceScreen> {
     return CupertinoPicker(
       itemExtent: 32,
       onSelectedItemChanged: (selectedIndex) {
-        //TODO: 6. Call the getCoinsValue, when an item is selected from the picker
         setState(() {
           selectedCurrency = currenciesList[selectedIndex];
-          //TODO: 5. Call the getCoinsValue, when an item is selected from the dropdown menu
           getCoinsValue();
         });
       },
@@ -91,7 +83,6 @@ class _PriceScreenState extends State<PriceScreen> {
           onChanged: (value) {
             setState(() {
               selectedCurrency = value!;
-              //TODO: 5. Call the getCoinsValue, when an item is selected from the dropdown menu
               getCoinsValue();
             });
           },
@@ -130,7 +121,6 @@ class _PriceScreenState extends State<PriceScreen> {
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         Image.asset(
-                          //TODO: 7. use toLowerCase function on icon to lower case the icon name
                           'images/${coinsList[index].icon.toLowerCase()}.png',
                           width: 60,
                         ),
